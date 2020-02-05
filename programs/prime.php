0584014536cb0e526bccc6bcbe0a9ba59fb7e6b9
2249abhi <abhishekshr7@gmail.com>
2020-02-05 21:35:35 +0530

    add programs

---
+++ programs/prime.php
@@ -0,0 +1,25 @@
+<?php
+//prime number
+
+
+error_reporting(E_ALL);
+
+$num =23;
+
+for( $j = 2; $j <= $num; $j++ )
+{
+	for( $k = 2; $k < $j; $k++ )
+	{
+		if( $j % $k == 0 )
+		{
+		break;
+		}
+
+	}
+
+if( $k == $j )
+echo "Prime Number : ". $j. "<br>";
+
+}
+
+?>
