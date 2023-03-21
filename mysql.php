782719a08b467903cb46a924f86e2e2c9f586056
2249abhi <33443146+2249abhi@users.noreply.github.com>
2023-03-21 19:38:34 +0530

    update questions

--- mysql.php
+++ mysql.php
@@ -2,4 +2,18 @@
 //all mysql query related questions
 
 //Ref: https://www.letsknowit.com/mysql-questions
+
+  What is difference between DBMS and RDBMS
+
+  How many types of JOINS in SQL
+    
+  How to get second maximum salary in an organization
+    
+  How to get the two max salary of an organization
+    
+  How to get the student name got maximum marks in a class
+    
+  What is difference between PRIMARY key and UNIQUE Key in sql
+  
+  
 ?>
