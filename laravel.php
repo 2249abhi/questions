60756badc9e1056ed600f1fba417b9fd443ca177
2249abhi <33443146+2249abhi@users.noreply.github.com>
2023-07-25 15:09:34 +0530

    Update laravel questions

--- laravel.php
+++ laravel.php
@@ -1,4 +1,21 @@
 <?php
 //all laravel related interview questions
 
+* composer
+* contracts
+* facade
+* Service Provider
+* Service Container
+* Eloquent
+* ORM
+* artisan
+* cache in laravel (redis, memcached)
+* Singleton design pattern
+* throttling
+* middleware
+* migration
+* seed
+* Laravel Request Life Cycle
+* prevent sql injection
+* scaffold
 ?>
