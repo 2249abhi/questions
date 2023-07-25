08d9656dc005cf743a013a05c7dab32cd7241293
2249abhi <33443146+2249abhi@users.noreply.github.com>
2023-07-25 15:23:08 +0530

    added oops questions

---
+++ oops/oops_questions.php
@@ -0,0 +1,23 @@
+<?php
+/*
+
+* Namespace
+* abstraction
+* access modifiers
+* constructor
+* encapsulation
+* final
+* Inheritance
+* Interface
+* magic functions
+* Polymorphism
+* Type of polymorphism (compile-time (overloading) and run time (overriding))
+* Static class
+* selfthis or self
+* traits
+* early and late binding
+
+
+
+*?
+?>
